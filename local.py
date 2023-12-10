9bfcd6ff91c7b1d8d6072ced31063c5f47f2c3b2
nacht-falter <johannes.bernet@posteo.de>
2023-12-10 02:56:44 +0100

    replace pygame with mpd/mpc for local audio playback

--- local.py
+++ local.py
@@ -1,63 +1,61 @@
 import os
-import pygame
 import sqlite3
 import json
 
 
 class AudioPlayer:
     def __init__(self, rfid, playback_state, location):
-        pygame.init()
         self.database_url = os.environ.get("DATABASE_URL")
         self.rfid = rfid
-        self.playback_state = json.loads(playback_state)
+        self.playback_state = (
+            json.loads(playback_state)
+            if playback_state
+            else {"track": 1, "position": "0%"}
+        )
         self.location = location
-        self.file_list = self.get_audio_files()
-        self.current_index = self.playback_state.get("current_index", 0)
         self.playing = False
 
-    def get_audio_files(self):
-        files = []
-        music_library = os.environ.get("MUSIC_LIBRARY")
-        for file in os.listdir(os.path.join(music_library, self.location)):
-            if file.endswith(".mp3") or file.endswith(".wav"):
-                files.append(os.path.join(music_library, self.location, file))
-        return files
-
     def play(self):
-        pygame.mixer.music.load(self.file_list[self.current_index])
-        pygame.mixer.music.play(
-            start=self.playback_state.get("position", 0) / 1000
-        )
+        os.system(f"mpc -q clear; mpc -q add {self.location}; mpc -q play")
+        os.system("mpc -q play " + str(self.playback_state["track"]))
+        os.system("mpc -q seek " + str(self.playback_state["position"]))
         self.playing = True
 
     def toggle_playback(self):
-        if self.playing:
-            pygame.mixer.music.pause()
-            self.playing = False
-        else:
-            pygame.mixer.music.unpause()
-            self.playing = True
+        os.system("mpc -q toggle")
+        self.playing = not self.playing
 
     def pause_playback(self):
-        pygame.mixer.music.pause()
+        os.system("mpc -q pause")
         self.playing = False
 
     def next_track(self):
-        self.current_index = (self.current_index + 1) % len(self.file_list)
-        self.play()
+        os.system("mpc -q next")
+        self.playing = True
 
     def previous_track(self):
-        self.current_index = (self.current_index - 1) % len(self.file_list)
-        self.play()
+        os.system("mpc -q prev")
+        self.playing = True
 
     def restart_playback(self):
+        self.playback_state = {"track": 1, "position": "0%"}
         self.play()
 
     def save_playback_state(self):
-        current_index = self.current_index
-        position = pygame.mixer.music.get_pos()
+        track_number = os.popen("mpc current -f %position%").read().strip()
+        mpc_status = os.popen("mpc status").readlines()
+        if len(mpc_status) > 1:
+            position = (
+                os.popen(
+                    "echo '" + mpc_status[1] + "' | awk -F '[()]' '{print $2}'"
+                )
+                .read()
+                .strip()
+            )
+        else:
+            position = "0%"
         self.playback_state = {
-            "current_index": current_index,
+            "track": track_number,
             "position": position,
         }
         with sqlite3.connect(self.database_url) as db:
