24f2d6a3cb4ea280319d35d29dc3ff4fa9cb901c
nacht-falter <johannes.bernet@posteo.de>
2023-12-20 21:46:25 +0100

    update button script

--- buttons.py
+++ buttons.py
@@ -6,4 +6,4 @@ GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def wait_for_power_button():
     GPIO.wait_for_edge(3, GPIO.FALLING)
-    os.system("systemctl poweroff")
+    os.system("sudo shutdown -h now")
