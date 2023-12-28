f7423471692e3e3c3ffa4f116a973c0b5efd18a8
nacht-falter <johannes.bernet@posteo.de>
2023-12-28 18:02:06 +0100

    add example for env.py

---
+++ example_env.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ["USERCREDS"] = "<ENCRYPTED USER CREDENTIALS FOR SPOTIFY PREMIUM ACCOUNT>"
+os.environ["REFRESH_TOKEN"] = "<SPOTIFY REFRESH TOKEN>"
+os.environ["DEVICE_ID"] = "<SPOTIFY DEVICE ID"
+os.environ["DATABASE_URL"] = "<DATABASE FILE OR URL>"
+os.environ["MUSIC_LIBRARY"] = "<MUSIC FOLDER URL>"
+os.environ["DEVELOPMENT"] = "False"
